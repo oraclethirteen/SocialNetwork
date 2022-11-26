19cf595cc6f045aed6af1cacef036d6e3a783d25
oraclethirteen <oracle.thirteen.o@gmail.com>
2022-11-26 18:52:32 +0300

    Add project files.

---
+++ SocialNetwork/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SocialNetwork
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MIddleName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+    }
+}
